2beb77f2184affc7f0851d7ffd2d23439bdb1dab
HagarRam <119937814+HagarRam@users.noreply.github.com>
2023-03-22 17:24:25 +0200

    with log in page!!

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,27 @@
 import React from 'react';
 import './App.css';
 import HomePage from './component/HomePage/HomePage';
-import {BrowserRouter, Route, Routes} from "react-router-dom";
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import LogInPage from './component/LogInPage/LogInPage';
 
-
-const App: React.FC= () =>  {
-  return (
-    <BrowserRouter>
-      <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/ShareSpace" />
-      <Route path="/Tips" />
-      <Route path="/LogIn" />
-      <Route path="/SignUp" />
-    </Routes>
-  </BrowserRouter>
-  );
-}
+const App: React.FC = () => {
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route
+					path="/"
+					element={<HomePage />}
+				/>
+				<Route path="/ShareSpace" />
+				<Route path="/Tips" />
+				<Route
+					path="/LogIn"
+					element={<LogInPage />}
+				/>
+				<Route path="/SignUp" />
+			</Routes>
+		</BrowserRouter>
+	);
+};
 
 export default App;
