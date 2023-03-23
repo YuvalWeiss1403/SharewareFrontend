aa8bb9d8ae0c8670ec96daf6369d82013884f583
HagarRam <119937814+HagarRam@users.noreply.github.com>
2023-03-23 16:09:24 +0200

    with tips!

--- src/component/TipsPage/SingleTip.tsx
+++ src/component/TipsPage/SingleTip.tsx
@@ -3,10 +3,18 @@ import Navbar from '../General/Navbar/Navbar';
 import { useSelector } from 'react-redux';
 import { RootState } from '../../store/store';
 import './singleTip.css';
-import heartwithout from './image/heartwithout.png';
-import fullHeart from './image/fullheart.png';
+// import heartwithout from './image/heartwithout.png';
+// import fullHeart from './image/fullheart.png';
+import like from './image/like.svg';
+import { useState } from 'react';
 const SingleTip: React.FC = () => {
 	const tipsData = useSelector((state: RootState) => state.tips.value);
+	const [number, setNumber] = useState<number>(0);
+	const handlePlus = (e: React.MouseEvent) => {
+		e.preventDefault();
+		setNumber(number + 1);
+	};
+	console.log(number);
 	return (
 		<div className="Tips-Page">
 			<div id="all-the-tips">
@@ -18,8 +26,15 @@ const SingleTip: React.FC = () => {
 							<div id="title">{data.title}</div>
 							<div id="username">{data.comment}</div>
 							<div id="username">{data.username}</div>
-							{/* <img src={heartwithout} />
-							<img src={fullHeart} /> */}
+							<div>
+								<img
+									src={like}
+									alt="like"
+									id="like"
+									onClick={handlePlus}
+								/>
+								<div>{data.likes}</div>
+							</div>
 							<div id="approved">{data.approved ? 'true' : ''}</div>
 						</div>
 					);
