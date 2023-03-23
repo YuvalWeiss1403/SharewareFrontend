1b2196536f9e52c539a4de92ef52d76fafe84ae5
HagarRam <119937814+HagarRam@users.noreply.github.com>
2023-03-23 11:17:19 +0200

    working! log in and log out

--- src/component/SignedInUser/SignedInUser.tsx
+++ src/component/SignedInUser/SignedInUser.tsx
@@ -17,7 +17,7 @@ const SignedInUser: React.FC = () => {
 	};
 
 	const user = JSON.parse(sessionStorage.getItem('user') || '');
-	const userName = user.first_name;
+	const userName = user.firstName;
 
 	return (
 		<div className="logIn-page">
