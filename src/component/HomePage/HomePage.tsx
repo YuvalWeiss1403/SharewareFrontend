1f22e0c77d9a0ca50bd37659b5a951de18a19968
rotem huga <rotem.3007@gmail.com>
2023-03-23 16:40:48 +0200

    adding the hero - style not finish - 23.03

--- src/component/HomePage/HomePage.tsx
+++ src/component/HomePage/HomePage.tsx
@@ -1,10 +1,12 @@
 import "../HomePage/HomePage.css"
 import Navbar from "../General/Navbar/Navbar"
+import ConceptHero from "../ConceptHero/ConceptHero"
 
 const HomePage: React.FC = () => {
 return (
     <div className="home-page">
         <Navbar />
+        <ConceptHero />
     </div>
 )
 }
