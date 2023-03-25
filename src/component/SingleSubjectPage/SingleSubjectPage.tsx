b422aa9b5e10e33c9130dc2b12bc61de7ab2903d
HagarRam <119937814+HagarRam@users.noreply.github.com>
2023-03-25 20:11:13 +0300

    DELETE QUESTION

--- src/component/SingleSubjectPage/SingleSubjectPage.tsx
+++ src/component/SingleSubjectPage/SingleSubjectPage.tsx
@@ -47,6 +47,33 @@ const SingleSubjectPage: React.FC = () => {
 		setCurrentQuestion(currentQuestion[0]);
 	};
 
+	const deleteQuestion = async (_id: ObjectId) => {
+		console.log(_id);
+		try {
+			const response = await fetch(`http://localhost:8000/questions`, {
+				method: 'DELETE',
+				body: JSON.stringify({
+					_id: _id,
+				}),
+				headers: {
+					'Content-type': 'application/json; charset=UTF-8',
+				},
+			});
+			const data = await response.json();
+			window.location.reload();
+			if (!response.ok) {
+				throw new Error(data.message);
+			}
+		} catch (err) {
+			console.error(err);
+			throw err;
+		}
+	};
+
+	const closeButton = async (id: ObjectId) => {
+		console.log(id);
+		await deleteQuestion(id);
+	};
 	return (
 		<div className="single-subject-page">
 			<Navbar />
@@ -63,17 +90,25 @@ const SingleSubjectPage: React.FC = () => {
 						</button>
 						{questionsData.map((question: IQuestions) => {
 							return (
-								<button
-									className={
-										currentQuestion === question
-											? 'questions button currentQ'
-											: 'questions button'
-									}
-									onClick={() => {
-										handleQuestionClick(question._id);
-									}}>
-									{question.header}
-								</button>
+								<div>
+									<span
+										id="closeButton"
+										onClick={() => closeButton(question._id)}
+										className="close">
+										&times;
+									</span>
+									<button
+										className={
+											currentQuestion === question
+												? 'questions button currentQ'
+												: 'questions button'
+										}
+										onClick={() => {
+											handleQuestionClick(question._id);
+										}}>
+										{question.header}
+									</button>
+								</div>
 							);
 						})}
 					</div>
