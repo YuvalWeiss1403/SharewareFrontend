14eb7dd5a9a39cd28654c4a6b844a906123bdfa4
Yuval Weiss <108930250+YuvalWeiss1403@users.noreply.github.com>
2023-03-27 01:28:49 +0300

    css update

--- src/component/ShareSpace/ShareSpace.tsx
+++ src/component/ShareSpace/ShareSpace.tsx
@@ -1,16 +1,16 @@
-import './ShareSpace.css';
-import Navbar from '../General/Navbar/Navbar';
-import { useSelector } from 'react-redux';
-import { RootState } from '../../store/store';
-import { ISubjects } from '../../store/slices/SubjectsSlice';
-import SubjectCard from './SubjectCard/SubjectCard';
-import { useState } from 'react';
-import AddSubject from '../AddSubject/AddSubject';
-import { ObjectId } from 'mongoose';
-import Modal from '../General/Modal/Modal';
+import "./ShareSpace.css";
+import Navbar from "../General/Navbar/Navbar";
+import { useSelector } from "react-redux";
+import { RootState } from "../../store/store";
+import { ISubjects } from "../../store/slices/SubjectsSlice";
+import SubjectCard from "./SubjectCard/SubjectCard";
+import { useState } from "react";
+import AddSubject from "../AddSubject/AddSubject";
+import { ObjectId } from "mongoose";
+import Modal from "../General/Modal/Modal";
 
 const ShareSpace: React.FC = () => {
-	const user = JSON.parse(sessionStorage.getItem('user') || '{}');
+	const user = JSON.parse(sessionStorage.getItem("user") || "{}");
 	console.log(user.userType);
 	const subjectsData = useSelector((state: RootState) => state.subjects.value);
 	const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
@@ -23,12 +23,12 @@ const ShareSpace: React.FC = () => {
 	const deleteSubject = async (_id: ObjectId) => {
 		try {
 			const response = await fetch(`http://localhost:8000/subjects`, {
-				method: 'DELETE',
+				method: "DELETE",
 				body: JSON.stringify({
 					_id: _id,
 				}),
 				headers: {
-					'Content-type': 'application/json; charset=UTF-8',
+					"Content-type": "application/json; charset=UTF-8",
 					Authorization: `Bearer ${user.token}`,
 				},
 			});
@@ -49,14 +49,17 @@ const ShareSpace: React.FC = () => {
 		<div className="ShareSpace">
 			<Navbar />
 			{/* {user.userType === 'admin' && ( */}
-				<button onClick={openModal}> ADD SUBJECT</button>
+			<button onClick={openModal} className="add-subject">
+				{" "}
+				ADD SUBJECT
+			</button>
 			{/* )} */}
 			{/* <button onClick={openModal}>Add Subject</button> */}
 			<div className="subjects-card-container">
 				{subjectsData?.map((subject: ISubjects, index: number) => {
 					return (
 						<div>
-							{user.userType === 'admin' && (
+							{user.userType === "admin" && (
 								<span
 									id="closeButton"
 									onClick={() => closeButton(subject._id)}
@@ -64,17 +67,18 @@ const ShareSpace: React.FC = () => {
 									&times;
 								</span>
 							)}
-							<SubjectCard
-								name={subject.name}
-								key={index}
-							/>
+							<SubjectCard name={subject.name} key={index} />
 						</div>
 					);
 				})}
 			</div>
 			{/* {isModalOpen && <AddSubject closeButton={closeModal} />} */}
-			{isModalOpen && <Modal isModalOpen={isModalOpen} setIsModalOpen={setIsModalOpen} > <AddSubject setIsModalOpen={setIsModalOpen} />
-			</Modal>}
+			{isModalOpen && (
+				<Modal isModalOpen={isModalOpen} setIsModalOpen={setIsModalOpen}>
+					{" "}
+					<AddSubject setIsModalOpen={setIsModalOpen} />
+				</Modal>
+			)}
 		</div>
 	);
 };
