dc91fb59a5e6b3e7a99bfc827cf2efb4cf63a15e
HagarRam <119937814+HagarRam@users.noreply.github.com>
2023-03-26 14:45:32 +0300

    with change

--- src/component/SingleSubjectPage/AnswerCard/AnswerCard.tsx
+++ src/component/SingleSubjectPage/AnswerCard/AnswerCard.tsx
@@ -1,9 +1,33 @@
-import { IAnswers } from "../../../store/slices/AnswersSlice";
-import "./AnswerCard.css";
+import { ObjectId } from 'mongoose';
+import { IAnswers } from '../../../store/slices/AnswersSlice';
+import './AnswerCard.css';
 
 export interface IAnswerCard {
 	answer: IAnswers;
 }
+
+const deleteAnswers = async (_id: ObjectId) => {
+	console.log(_id);
+	try {
+		const response = await fetch(`http://localhost:8000/answers`, {
+			method: 'DELETE',
+			body: JSON.stringify({
+				_id: _id,
+			}),
+			headers: {
+				'Content-type': 'application/json; charset=UTF-8',
+			},
+		});
+		const data = await response.json();
+		window.location.reload();
+		if (!response.ok) {
+			throw new Error(data.message);
+		}
+	} catch (err) {
+		console.error(err);
+		throw err;
+	}
+};
 
 const AnswerCard: React.FC<IAnswerCard> = (props: IAnswerCard) => {
 	const currentAnswer = props.answer;
