9cc36ac1144dbd7b9c9cd906b97a09fecae105db
Yuval Weiss <108930250+YuvalWeiss1403@users.noreply.github.com>
2023-03-27 16:13:22 +0300

    flow HP content

--- src/component/FlowHP/FlowHP.tsx
+++ src/component/FlowHP/FlowHP.tsx
@@ -1,32 +1,41 @@
-import "../FlowHP/FlowHP.css"
+import "../FlowHP/FlowHP.css";
 
 const FlowHP: React.FC = () => {
-    return (
-        <div className="all-flow-container">
-            <div className="all-flow-dashboard">
-                <hr/>
-                <div className="home-page-container">
-                    <span>1</span>
-                    <div className="home-page-content">
-                        <div className="heading">Home Page</div>
-                    </div>
-                </div>
-                <div className="all-shareSpace-container">
-                    <span>2</span>
-                    <div className="all-shareSpace-content">
-                        <div className="heading">All ShareSpace</div>
-                    </div>
-                </div>
-                <div className="tips-container-hp">
-                    <span>3</span>
-                    <div className="tips-content-hp">
-                    <div className="heading">Tips And Advices</div>
-                    </div>
-                </div> 
-            </div>
-        </div>
-    )
-}
+	return (
+		<div className="all-flow-container">
+			<div className="all-flow-dashboard">
+				<hr />
+				<div className="home-page-container">
+					<span>?</span>
+					<div className="home-page-content">
+						<div className="heading">Asking Questions</div>
+						<div className="flow-box-content">
+							Working on a share space where you can answer team mates questions
+						</div>
+					</div>
+				</div>
+				<div className="all-shareSpace-container">
+					<span>@</span>
+					<div className="all-shareSpace-content">
+						<div className="heading">Sharing your Solutions</div>
+						<div className="flow-box-content">
+							Uploading questions that will be connected to a specific subject
+							to help you look it up after{" "}
+						</div>
+					</div>
+				</div>
+				<div className="tips-container-hp">
+					<span>$</span>
+					<div className="tips-content-hp">
+						<div className="heading">Sharing Tips and Advices</div>
+						<div className="flow-box-content">
+							Sharing your tips and advices with your friends
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
 
-
-export default FlowHP
+export default FlowHP;
