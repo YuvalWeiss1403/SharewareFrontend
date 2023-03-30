b5553484a83ca5604bb445607e64011c33e5f0e1
rotem huga <rotem.3007@gmail.com>
2023-03-30 16:37:18 +0300

    styling the no access page

--- src/component/NotFoundPage/NotFoundPage.tsx
+++ src/component/NotFoundPage/NotFoundPage.tsx
@@ -2,16 +2,20 @@ import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import Footer from '../General/Footer/Footer';
 import Navbar from '../General/Navbar/Navbar';
+import notFound from "../../assets/icons/not-found.png"
+import "../NotFoundPage/NotFoundPage.css"
 
 const NotFoundPage: React.FC = () => {
 	const navigate = useNavigate();
 	return (
-		<div>
-			<button className="not-found">not found</button>
+		<div className='no-access-container'>
+			<button className="not-found">You Are Not Allowed To Access This Page</button>
+			<img src={notFound} alt="no-access-icon" className='no-access-icon' />
 			<button
-				id="homepage"
+				// id="homepage"
+				className="to-hp-button"
 				onClick={() => navigate('/')}>
-				to home page
+				To Home Page
 			</button>
 		</div>
 	);
