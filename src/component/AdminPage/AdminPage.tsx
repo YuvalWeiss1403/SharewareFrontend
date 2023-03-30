9e3f800e7de7645e8af9852a9ceaf5d22184d2f7
rotem huga <rotem.3007@gmail.com>
2023-03-30 16:17:38 +0300

    fixind the admin page style include mobile

--- src/component/AdminPage/AdminPage.tsx
+++ src/component/AdminPage/AdminPage.tsx
@@ -39,29 +39,32 @@ const AdminPage: React.FC = () => {
 				{user.userType === 'admin' && (
 					<div className="add-students-container">
 						<div className="add-students-title">Add Students Details</div>
-						<input
-							value={User.first_name}
-							onChange={(e) => setUser({ ...User, first_name: e.target.value })}
-							className="first-name-input"
-							type="text"
-							placeholder="First Name"></input>
+						<div className='all-inputs'>
+							<input
+								value={User.first_name}
+								onChange={(e) => setUser({ ...User, first_name: e.target.value })}
+								className="first-name-input"
+								type="text"
+								placeholder="First Name"></input>
 
-						<input
-							value={User.last_name}
-							onChange={(e) => setUser({ ...User, last_name: e.target.value })}
-							className="last-name-input"
-							type="text"
-							placeholder="Last Name"></input>
-						<input
-							value={User.email}
-							onChange={async (e) =>
-								await setUser({ ...User, email: e.target.value })
-							}
-							className="email-input"
-							type="email"
-							placeholder="Email address"></input>
+							<input
+								value={User.last_name}
+								onChange={(e) => setUser({ ...User, last_name: e.target.value })}
+								className="last-name-input"
+								type="text"
+								placeholder="Last Name"></input>
+							<input
+								value={User.email}
+								onChange={async (e) =>
+									await setUser({ ...User, email: e.target.value })
+								}
+								className="email-input"
+								type="email"
+								placeholder="Email address">
+							</input>
+						</div>
 						<button
-							id="page-sign-up-button"
+							className="admin-page-sign-up-button"
 							onClick={(e) => {
 								e.preventDefault();
 								AdminSign();
