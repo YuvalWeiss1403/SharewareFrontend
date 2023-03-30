535cab1083856a29dcb014c3e8015e29feadcd9b
HagarRam <119937814+HagarRam@users.noreply.github.com>
2023-03-30 13:32:45 +0300

    WITH ADMINPAGE

--- src/component/AdminPage/AdminPage.tsx
+++ src/component/AdminPage/AdminPage.tsx
@@ -1,26 +1,68 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router';
 import Footer from '../General/Footer/Footer';
 import Navbar from '../General/Navbar/Navbar';
 
 const AdminPage: React.FC = () => {
+	const defaultInputValue = {
+		first_name: '',
+		last_name: '',
+		email: '',
+	};
+	const [User, setUser] = useState(defaultInputValue);
+	const navigator = useNavigate();
+
+	const AdminSign = async () => {
+		try {
+			const userReq = await axios.post(
+				'http://localhost:8000/adminUser/create',
+				{
+					firstName: User.first_name,
+					lastName: User.last_name,
+					email: User.email,
+				}
+			);
+			navigator('/');
+		} catch (error: any) {
+			alert(error.response.data);
+			return [];
+		}
+	};
+
 	return (
 		<div>
 			<Navbar />
 			<form>
-				<div>
-					<input
-						type={'text'}
-						placeholder={'first Name'}
-					/>
-					<input
-						type={'text'}
-						placeholder={'Last Name'}
-					/>
-					<input
-						type={'text'}
-						placeholder={'email'}
-					/>
-				</div>
+				<input
+					value={User.first_name}
+					onChange={(e) => setUser({ ...User, first_name: e.target.value })}
+					className="enter-first-name"
+					type="text"
+					placeholder="First Name"></input>
+				<input
+					value={User.last_name}
+					onChange={(e) => setUser({ ...User, last_name: e.target.value })}
+					className="enter-last-name"
+					type="text"
+					placeholder="Last Name"></input>
+				<input
+					value={User.email}
+					onChange={async (e) =>
+						await setUser({ ...User, email: e.target.value })
+					}
+					className="enter-email"
+					type="email"
+					placeholder="Email address"></input>
+				<button
+					id="page-sign-up-button"
+					onClick={(e) => {
+						e.preventDefault();
+						AdminSign();
+						setUser(defaultInputValue);
+					}}>
+					sign up
+				</button>
 			</form>
 			<Footer />
 		</div>
