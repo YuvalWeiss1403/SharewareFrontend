8c42c74680263e2cd7f40b74bfffa576bad008e2
rotem huga <rotem.3007@gmail.com>
2023-03-30 13:49:52 +0300

    styling the students details page - 30.03

--- src/component/AdminPage/AdminPage.tsx
+++ src/component/AdminPage/AdminPage.tsx
@@ -1,24 +1,29 @@
 import React from 'react';
 import Footer from '../General/Footer/Footer';
 import Navbar from '../General/Navbar/Navbar';
+import '../AdminPage/AdminPage.css'
 
 const AdminPage: React.FC = () => {
 	return (
 		<div>
 			<Navbar />
-			<form>
-				<div>
+			<form className='form-container'>
+				<div className='add-students-container'>
+					<div className='add-students-title'>Add Students Details</div>
 					<input
 						type={'text'}
 						placeholder={'first Name'}
+						className= "first-name-input"
 					/>
 					<input
 						type={'text'}
 						placeholder={'Last Name'}
+						className= "user-name-input"
 					/>
 					<input
 						type={'text'}
 						placeholder={'email'}
+						className= "email-input"
 					/>
 				</div>
 			</form>
