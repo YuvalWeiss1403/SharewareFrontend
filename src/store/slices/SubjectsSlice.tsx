5f712a3ba38fd847401a299d6a511bc3f44b4a11
Yuval Weiss <108930250+YuvalWeiss1403@users.noreply.github.com>
2023-04-02 13:15:12 +0300

    update subject slice url

--- src/store/slices/SubjectsSlice.tsx
+++ src/store/slices/SubjectsSlice.tsx
@@ -1,5 +1,5 @@
-import { createSlice } from '@reduxjs/toolkit';
-import { ObjectId } from 'mongoose';
+import { createSlice } from "@reduxjs/toolkit";
+import { ObjectId } from "mongoose";
 
 export interface ISubjects {
 	toLowerCase(): unknown;
@@ -9,9 +9,12 @@ export interface ISubjects {
 
 const dataSubject = async () => {
 	try {
-		const response = await fetch('http://localhost:8000/subjects', {
-			method: 'GET',
-		});
+		const response = await fetch(
+			"https://shareware-server.onrender.com/subjects",
+			{
+				method: "GET",
+			}
+		);
 		const data = await response.json();
 		return data;
 	} catch (err) {
@@ -20,7 +23,7 @@ const dataSubject = async () => {
 };
 const subjects: ISubjects[] = await dataSubject();
 export const subjectsSlice = createSlice({
-	name: 'Subjects',
+	name: "Subjects",
 	initialState: {
 		value: subjects,
 		filteredValue: {},
